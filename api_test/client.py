b15cdf5f48d07a360140789fb8a39220bdeee35e
iolivero <80908962+iolivero@users.noreply.github.com>
2022-11-02 15:17:26 -0300

    Update client.py
    commit

--- api_test/client.py
+++ api_test/client.py
@@ -1,13 +1,15 @@
 import requests
 import json
- url = 'http://localhost:5000'
-  data.
-  data = requests.get(url)
-  if data.status_code == 200:
-     data = data.json()
+url = 'http://localhost:5000/personas'
+data = requests.get(url)
+if data.status_code == 200:
+   data = data.json()
+else:
+   print("error")
+   exit(1)
 
-    for e in data[payload]
-         print(e['payload'])
-      
+for person in data:
+   print(f'''La persona {person["nombres"]} {person["apellido_paterno"]} {person["apellido_materno"]}
+tiene id: {person["id"]} y correo: {person["email"]}''')
 
 
